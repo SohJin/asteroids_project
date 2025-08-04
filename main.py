2f5e42617ce79bc4cd54f82ac4ccbf4710b09d7e
SohJin <negadragan@hotmail.com>
2025-08-04 11:20:01 -0500

    the screen runs on a Clock and .tick() cycle

--- main.py
+++ main.py
@@ -10,12 +10,16 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    game_clock = pygame.time.Clock()
+    dt = 60
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen, color=(0,0,0))
         pygame.display.flip()
+        game_clock.tick(60)
+        dt = game_clock.tick(60)
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
