2357b7d9d506b76e4c910be754ca9680c3180f6e
SohJin <negadragan@hotmail.com>
2025-08-04 11:48:20 -0500

    updated constants, added player functionality, game screen now draws a triangle

--- main.py
+++ main.py
@@ -3,20 +3,23 @@
 # throughout this file
 import pygame
 
-# import everything from the module
-# database.py into the current file
+# import everything from the following .py
+# modules into the current file
 from constants import *
+from player import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     game_clock = pygame.time.Clock()
     dt = 60
+    poor_soul = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen, color=(0,0,0))
+        poor_soul.draw(screen)
         pygame.display.flip()
         game_clock.tick(60)
         dt = game_clock.tick(60)
