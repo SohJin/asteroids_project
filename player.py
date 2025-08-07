d39f27169ecb7759d75ca8ebb4d588669269c990
SohJin <negadragan@hotmail.com>
2025-08-07 09:07:13 -0500

    the player can move now

--- player.py
+++ player.py
@@ -25,6 +25,15 @@ class Player(CircleShape):
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_a]:
-            self.rotate(-1 / 60)
+            self.rotate((-1 / 60))
         if keys[pygame.K_d]:
-            self.rotate(1 / 60)
+            self.rotate((1 / 60))
+        
+        if keys[pygame.K_w]:
+            self.move((1 / 60))
+        if keys[pygame.K_s]:
+            self.rotate((-1 / 60))
+
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
